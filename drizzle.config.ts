5491760d672c7a173dbce9c7d6b96c40f5fdf83c
unknown <aminezouguig@proton.me>
2024-09-08 02:30:58 +0200

    Setting Up Google and Github Providers, Save Users With Drizzle Adapter to your DB

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-// Load environment variables from the `.env.local` file
-dotenv.config({ path: ".env.local" });
+dotenv.config({
+  path: ".env.local",
+});
 
-// Ensure POSTGRES_URL is correctly set
-if (!process.env.POSTGRES_URL) {
-  throw new Error("POSTGRES_URL environment variable is not defined");
-}
-
-// Export the configuration
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./server/schema.ts",
   out: "./server/migrations",
-  driver: "pg",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL, // Ensure that POSTGRES_URL is set
+    url: process.env.POSTGRES_URL!,
   },
 });
