7ac5399bcb404802fedece7e596148f831c45582
unknown <aminezouguig@proton.me>
2024-09-10 12:05:14 +0200

    feat: create register page

--- app/components/auth/register-form.tsx
+++ app/components/auth/register-form.tsx
@@ -12,47 +12,55 @@ import {
 } from "../ui/form";
 import { AuthCard } from "./auth-card";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginSchema } from "@/types/login-schema";
+import { RegisterSchema } from "@/types/register-schema";
 import * as z from "zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { emailSignIn } from "@/server/actions/email-signin";
 import { useAction } from "next-safe-action/hooks";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 
-const LoginForm = () => {
+const RegisterForm = () => {
   const form = useForm({
-    resolver: zodResolver(LoginSchema),
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
   const [error, setError] = useState("");
 
-  const { execute, status, result } = useAction(emailSignIn, {
-    onSuccess(data) {
-      console.log(data);
-    },
-  });
-
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    execute(values);
-  };
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {};
   return (
     <AuthCard
-      cardTitle="Welcome Back!"
-      backButtonHref="/auth/register"
-      backButtonLabel="Create a new account"
+      cardTitle="Create an account"
+      backButtonHref="/auth/login"
+      backButtonLabel="Already have an account?"
       showSocials
     >
       <div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
             <div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+
+                    <FormLabel />
+                    <FormControl>
+                      <Input {...field} placeholder="developedby" type="text" />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <FormField
                 control={form.control}
                 name="email"
@@ -65,8 +73,8 @@ const LoginForm = () => {
                       <Input
                         {...field}
                         placeholder="example@gmail.com"
-                        autoComplete="email"
                         type="email"
+                        autoComplete="email"
                       />
                     </FormControl>
                     <FormDescription />
@@ -86,8 +94,8 @@ const LoginForm = () => {
                       <Input
                         {...field}
                         placeholder="*****************"
-                        autoComplete="current-password"
                         type="password"
+                        autoComplete="current-password"
                       />
                     </FormControl>
                     <FormDescription />
@@ -106,7 +114,7 @@ const LoginForm = () => {
                 status === "executing" ? "animate-pulse" : ""
               )}
             >
-              {"Login"}
+              Register
             </Button>
           </form>
         </Form>
@@ -115,4 +123,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
