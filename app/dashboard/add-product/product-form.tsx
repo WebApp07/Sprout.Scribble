2ea64a7c5b7a4d23a3a77b0aec46749937dc5927
unknown <aminezouguig@proton.me>
2024-10-28 01:57:47 +0100

    Admin Dashboard Create Product Form

---
+++ app/dashboard/add-product/product-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zProductSchema } from "@/types/product-schema";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DollarSign } from "lucide-react";
+import Tiptap from "./tiptap";
+
+export default function ProductForm() {
+  const form = useForm<zProductSchema>({
+    defaultValues: {
+      title: "",
+      description: "",
+      price: 0,
+    },
+  });
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={() => console.log("hey")} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Saekdong Stripe" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Tiptap val={field.value} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem className="py-2">
+                  <FormLabel>Product Price</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-2">
+                      <DollarSign
+                        size={36}
+                        className="p-2 bg-muted rounded-md"
+                      />
+                      <Input
+                        {...field}
+                        type="number"
+                        placeholder="Your price in USD"
+                        step="0.1"
+                        min={0}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
